37ba1f8074d2e15f2e556b80d428c9274cf9def8
DineshBabu-Git <dinesheee144@gmail.com>
2025-11-19 21:56:08 +0530

    File Updated

--- src/services/omdb.jsx
+++ src/services/omdb.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = "http://www.omdbapi.com/";
+const BASE_URL = "https://www.omdbapi.com/";
 const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
 
 async function fetchFromOmdb(params) {
